217e205889e72590b010d8e151aa6a5a876f682e
AYALA HAIM <ay4117096@gmail.com>
2022-10-30 16:01:42 +0200

    initial code+welcome function

---
+++ dotNet5783_2453_2271/stage0/Program2271.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] agrs)
+        {
+            welcome2271();
+            welcome2453();
+            Console.ReadKey();
+        }
+
+        static partial void welcome2453();
+        private static void welcome2271()
+        {
+            Console.WriteLine("Enter your name: ");
+            string username = Console.ReadLine();
+            Console.WriteLine("{ 0}, welcome to my first console application", username);
+        }
+
+    }
+}
