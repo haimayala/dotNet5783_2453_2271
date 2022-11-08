ed6aeaa30f0124cddb27d13a98308203e1937bcc
ENH000 <hashayeden@gmail.com>
2022-11-08 16:56:37 +0200

    main in dalltest->program

--- dotNet5783_2453_2271/DalTest/Program.cs
+++ dotNet5783_2453_2271/DalTest/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+namespace Targil0
+{
+    partial class Program
+    {
+        enum Menu
+        {
+            EXIT, CHECKORDER, CHECKORDERITEM, CHECKPRODUCT
+        }
+        enum Options
+        {
+            addToList = 'a', showById = 'b', showByDall = 'c', update = 'd', delete = 'e';
+        }
+        static void Main()
+        {
+            int x;
+            x = System.Console.Read();
+
+            switch (x)
+            {
+                case (int)Menu.EXIT:
+                    {
+                        char option = (char)System.Console.Read();
+                        switch (option)
+                        {
+                            case (char)Options.addToList:
+
+                            default:
+                                break;
+                        }
+                        break;
+                    }
+                default:
+                    break;
+            }
+        }
+    }
+}
